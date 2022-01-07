35d70ea1e7a0dd13ba4bbbeb4a52ab47fd85d926
FilopateerMaged <44089932+FilopateerMaged@users.noreply.github.com>
2022-01-07 18:07:54 +0200

    PST zip script v1

---
+++ Office 365/Zipping Pst files.ps1
@@ -0,0 +1,23 @@
+﻿
+$Folder = "PST File Destenation should end with \*.pst"
+
+$Backup = "Zip file destenation"
+
+$compress = @{
+  Path = $Folder
+  CompressionLevel = "Fastest"
+  DestinationPath = $Backup
+}
+
+
+# Closing all instances of outlook before proceeding
+
+Get-Process Outlook |   Foreach-Object { $_.CloseMainWindow() | Out-Null } | stop-process –force
+
+# Zipping all pst files to specified destination to one zip file
+
+Compress-Archive @compress
+
+#Starting outlook again
+
+Start-Process Outlook
